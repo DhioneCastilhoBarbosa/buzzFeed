087fc1896a45232350ddf858ae4d0bead15c41b6
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2024-02-23 13:39:32 -0300

    rules of game implemented

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-quizz',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
 
+  title: string = ""
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+  questionIndex:number = 0
+  questionMaxIndex:number =0
+
+  finished:boolean = false
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playrChoose(value:string){
+    this.answers.push(value)
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+    if(this.questionMaxIndex>this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      this.finished = true
+    }
+  }
 }
